520fdd3688465586430e5717bb773bd16bea77f6
Dominic Schira <domshyra@gmail.com>
2016-09-19 17:46:30 -0700

    connect signup with Touch Up Inside

--- JustAnotherMessagingService/LoginViewController.swift
+++ JustAnotherMessagingService/LoginViewController.swift
@@ -20,6 +20,8 @@ class LoginViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func unwindToLogInScreen(segue:UIStoryboardSegue) {
+    }
     /*
      // MARK: - Navigation
      
--- JustAnotherMessagingService/SignUpViewController.swift
+++ JustAnotherMessagingService/SignUpViewController.swift
@@ -26,7 +26,57 @@ class SignUpViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    
+    @IBAction func signUpAction(sender: AnyObject) {
+        
+        var username = self.usernameField.text
+        var password = self.passwordField.text
+        var email = self.emailField.text
+//        var finalEmail = email.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet())
+//        
+//        // Validate the text fields
+//        if count(username) < 5 {
+//            var alert = UIAlertView(title: "Invalid", message: "Username must be greater than 5 characters", delegate: self, cancelButtonTitle: "OK")
+//            alert.show()
+//            
+//        } else if count(password) < 8 {
+//            var alert = UIAlertView(title: "Invalid", message: "Password must be greater than 8 characters", delegate: self, cancelButtonTitle: "OK")
+//            alert.show()
+//            
+//        } else if count(email) < 8 {
+//            var alert = UIAlertView(title: "Invalid", message: "Please enter a valid email address", delegate: self, cancelButtonTitle: "OK")
+//            alert.show()
+//            
+//        } else {
+//            // Run a spinner to show a task in progress
+//            var spinner: UIActivityIndicatorView = UIActivityIndicatorView(frame: CGRectMake(0, 0, 150, 150)) as UIActivityIndicatorView
+//            spinner.startAnimating()
+//            
+//            var newUser = PFUser()
+//            
+//            newUser.username = username
+//            newUser.password = password
+//            newUser.email = finalEmail
+//            
+//            // Sign up the user asynchronously
+//            newUser.signUpInBackgroundWithBlock({ (succeed, error) -> Void in
+//                
+//                // Stop the spinner
+//                spinner.stopAnimating()
+//                if ((error) != nil) {
+//                    var alert = UIAlertView(title: "Error", message: "\(error)", delegate: self, cancelButtonTitle: "OK")
+//                    alert.show()
+//                    
+//                } else {
+//                    var alert = UIAlertView(title: "Success", message: "Signed Up", delegate: self, cancelButtonTitle: "OK")
+//                    alert.show()
+//                    dispatch_async(dispatch_get_main_queue(), { () -> Void in
+//                        let viewController:UIViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("Home") as! UIViewController
+//                        self.presentViewController(viewController, animated: true, completion: nil)
+//                    })
+//                }
+//            })
+//        }
+    }
     /*
      // MARK: - Navigation
      
