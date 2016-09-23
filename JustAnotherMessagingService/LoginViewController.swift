3b5dae7927e69fad63958b855dd73054c239b1c3
Dominic Schira <domshyra@gmail.com>
2016-09-22 21:15:39 -0700

    link elements to IBOutlets in view controllers

--- JustAnotherMessagingService/LoginViewController.swift
+++ JustAnotherMessagingService/LoginViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
