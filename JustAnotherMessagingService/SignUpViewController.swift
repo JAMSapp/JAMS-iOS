c0b39d3dd7077456ecf54e925d76a47bbcfe36e4
Dominic Schira <domshyra@gmail.com>
2016-09-25 20:06:39 -0700

    Register (#3)
    * register page elements set up
    
    * add loginviewcontroller and signupveiwcontroller
    
    * change elements
    
    * connect each of the outlet variables to the corresponding text fields
    
    * connect signup with Touch Up Inside
    
    * Add return button constraints for text fields.
    
    * remove sample ui elements to create new ones without constraint conflicts
    
    * Add basic ui elements into place
    
    * link elements to IBOutlets in view controllers
    
    * Constraints set up for iPhone so that ui elements stay centered
    
    * Sign up view controller now has login button in case user already has an account.

---
+++ JustAnotherMessagingService/SignUpViewController.swift
@@ -0,0 +1,40 @@
+//
+//  SignUpViewController.swift
+//  JustAnotherMessagingService
+//
+//  Created by Dominic Schira on 9/19/16.
+//  Copyright © 2016 Dominic Schira. All rights reserved.
+//
+
+import UIKit
+
+
+class SignUpViewController: UIViewController {
+    
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+/* sign up button logic will go here */
+    
+//    @IBAction func signUpAction(sender: AnyObject) {
+//        
+//        var username = self.usernameField.text
+//        var password = self.passwordField.text
+//        var email = self.emailField.text
+//
+//    
+//}
+
+}
