eb669421ff3dc64db5df487929fe223084f23bdd
Dominic Schira <domshyra@gmail.com>
2016-10-06 18:57:34 -0700

    User creation variable is now named CreateUserPath

--- JustAnotherMessagingService/Constants.swift
+++ JustAnotherMessagingService/Constants.swift
@@ -10,6 +10,6 @@ import Foundation
 
 struct K {
     struct URL {
-        static let Path = "http://192.168.1.124:8080/api/user"
+        static let CreateUserPath = "https://#/api/user"
     }
 }
